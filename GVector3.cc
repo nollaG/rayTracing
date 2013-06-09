185315bc0a3ee5dd937cb2506793c5c6ddcdbd79
pycq2002 <pycq2002@gmail.com>
2013-06-09 20:27:06 +0800

    Initial Commit,complete GVector3,not test

---
+++ GVector3.cc
@@ -0,0 +1,93 @@
+#include "inc/GVector3.h"
+#include <cmath>
+#include <iostream>
+
+GVector3::GVector3():x(0.0f),y(0.0f),z(0.0f){}
+
+GVector3::GVector3(float xx,float yy,float zz):x(xx),y(yy),z(zz){}
+
+float GVector3::length() {
+  return sqrt(x*x+y*y+z*z);
+}
+
+bool GVector3::normalize() {
+  float len=this->length();
+  if (len==0)
+    return false;
+  x/=len;
+  y/=len;
+  z/=len;
+  return true;
+}
+
+GVector3 GVector3::operator+(const GVector3& rhs) const {
+  GVector3 g1(x+rhs.x,y+rhs.y,z+rhs.z);
+  return g1;
+}
+GVector3 GVector3::operator+=(const GVector3& rhs) {
+  x+=rhs.x;
+  y+=rhs.y;
+  z+=rhs.z;
+  return *this;
+}
+GVector3 GVector3::operator-(const GVector3& rhs) const {
+  GVector3 g1(x-rhs.x,y-rhs.y,z-rhs.z);
+  return g1;
+}
+GVector3 GVector3::operator-=(const GVector3& rhs) {
+  x-=rhs.x;
+  y-=rhs.y;
+  z-=rhs.z;
+  return *this;
+}
+
+GVector3 GVector3::operator*(const float f) const {
+  GVector3 g1(x*f,y*f,z*f);
+  return g1;
+}
+GVector3 GVector3::operator*=(const float f) {
+  x*=f;
+  y*=f;
+  z*=f;
+  return *this;
+}
+
+GVector3 GVector3::operator*(const GVector3& rhs) const {
+  GVector3 g1(x*rhs.x,y*rhs.y,z*rhs.z);
+  return g1;
+}
+
+GVector3 GVector3::operator*=(const GVector3& rhs) {
+  x*=rhs.x;
+  y*=rhs.y;
+  z*=rhs.z;
+  return *this;
+}
+
+
+GVector3 GVector3::operator/(const float f) const {
+  GVector3 g1;
+  if (f==0.0f)
+    return g1;
+  g1.x=x/f;
+  g1.y=y/f;
+  g1.z=z/y;
+  return g1;
+}
+GVector3 GVector3::operator/=(const float f) {
+  if (f==0.0f)
+    return *this;
+  x/=f;
+  y/=f;
+  z/=f;
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os,const GVector3& g) {
+  os << g.x << g.y << g.z;
+  return os;
+}
+std::istream& operator>>(std::istream& is,GVector3& g) {
+  is >> g.x >> g.y >> g.z;
+  return is;
+}
---
+++ inc/GVector3.h
@@ -0,0 +1,33 @@
+#ifndef GVECTOR3_H
+
+#define GVECTOR3_H
+#include <iostream>
+
+class GVector3 {
+  public:  
+    float x;
+    float y;
+    float z;
+  public:
+    friend std::ostream& operator<<(std::ostream,const GVector3&);
+    friend std::istream& operator>>(std::istream,GVector3&);
+    GVector3();
+    GVector3(float,float,float);
+    float length();
+    //false if the length==0
+    bool normalize();
+    GVector3 operator+(const GVector3&) const;
+    GVector3 operator+=(const GVector3&);
+    GVector3 operator-(const GVector3&) const;
+    GVector3 operator-=(const GVector3&);
+    GVector3 operator*(const float) const;
+    GVector3 operator*=(const float);
+    GVector3 operator*(const GVector3&) const;
+    GVector3 operator*=(const GVector3&);
+    GVector3 operator/(const float) const;
+    GVector3 operator/=(const float);
+};
+std::ostream& operator<<(std::ostream&,const GVector3&);
+std::istream& operator>>(std::istream&,GVector3&);
+
+#endif /* end of include guard: GVECTOR3_H */
